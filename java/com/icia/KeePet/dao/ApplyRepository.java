f6d40594e9e6aa65bd6098d7163af664b7e0d36f
수연 <javapyhtml@gmail.com>
2025-02-17 09:16:31 +0900

    Create ApplyRepository.java

---
+++ java/com/icia/KeePet/dao/ApplyRepository.java
@@ -0,0 +1,38 @@
+package com.icia.rmate.dao;
+
+import com.icia.rmate.dto.ApplyEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ApplyRepository extends JpaRepository<ApplyEntity, Integer> {
+    @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN 1 ELSE 0 END FROM APPLY WHERE B_NUM = :bNum AND USER_ID = :userId", nativeQuery = true)
+    Integer existsByBNumAndUserId(@Param("bNum") int bNum, @Param("userId") String userId);
+
+
+    @Query("SELECT a FROM ApplyEntity a WHERE a.BNum IN :bNumList")
+    List<ApplyEntity> findByBNumIn(@Param("bNumList") List<Integer> bNumList);
+
+    @Query("SELECT a FROM ApplyEntity a WHERE a.UserId = :UserId")
+    List<ApplyEntity> findByUserId(@Param("UserId") String UserId);
+
+    @Query("SELECT a FROM ApplyEntity a WHERE a.BNum = :bNum")
+    List<ApplyEntity> findByBNumAndAStatusOne(@Param("bNum") int bNum); // bNum으로 여러 ApplyEntity 조회 (a_status = 1)
+
+//    @Query("SELECT a FROM ApplyEntity a WHERE a.BNum = :bNum AND a.AStatus = 1")
+//    List<ApplyEntity> findByBNumAndAStatus(@Param("bNum") int bNum);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE ApplyEntity a SET a.RStatus = 1 WHERE a.BNum = :bNum and a.UserId = :UserId")
+    void saveRStatus(@Param("bNum") int bNum,@Param("UserId") String UserId);
+
+
+    // 전체 신청 내역을 조회하는 메서드
+    List<ApplyEntity> findAll();
+
+}
